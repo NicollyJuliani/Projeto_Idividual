f4ef76bd714101ebecc8ac9f0cde5f06a8bfc6c1
Nicolly Juliani <nicollyjuliane2004@gmail.com>
2023-05-14 21:11:44 -0300

    atualização da index

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".slide-contente", {
+    slidesPerView: 4,
+    centeredSlides: true,
+    spaceBetween: 30,
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
