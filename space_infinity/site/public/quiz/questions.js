2c62813da7b899719119c60a3a1330b627925490
Nicolly Juliani <nicollyjuliane2004@gmail.com>
2023-05-19 11:41:14 -0300

    ip

---
+++ space_infinity/site/public/quiz/questions.js
@@ -0,0 +1,49 @@
+// Armazenando as questões em um array
+
+export default [
+  {
+    question: "Qual é o objeto mais massivo do universo conhecido?",
+    answers: [
+      { option: "Buraco Negro", correct: false },
+      { option: "Galáxia", correct: true },
+      { option: "Planeta Júpiter", correct: false },
+      { option: "Planeta Júpiter", correct: false },
+    ],
+  },
+  {
+    question: "Qual é a idade aproximada do universo?",
+    answers: [
+      { option: "100 milhões de anos", correct: false },
+      { option: "4,5 bilhões de anos", correct: false },
+      { option: "13,8 bilhões de anos", correct: true },
+      { option: "13,8 bilhões de anos", correct: false },
+    ],
+  },
+  {
+    question: "Qual é a temperatura média do espaço intergaláctico?",
+    answers: [
+      { option: "-270 graus Celsius", correct: true },
+      { option: "-100 graus Celsius", correct: false },
+      { option: " 20 graus Celsius", correct: false },
+      { option: " 20 graus Celsius", correct: false },
+    ],
+  },
+  {
+    question: "Qual é a teoria científica aceita sobre a formação do universo?",
+    answers: [
+      { option: "Teoria do Big Bang", correct: true },
+      { option: "Teoria da Terra Plana", correct: false },
+      { option: "Teoria da Terra Oca", correct: false },
+      { option: "Teoria da Terra Oca", correct: false },
+    ],
+  },
+  {
+    question: "Qual é a estrela mais próxima do nosso sistema solar?",
+    answers: [
+      { option: "Proxima Centauri", correct: true },
+      { option: "Alpha Centauri A", correct: false },
+      { option: "Alpha Centauri B", correct: false },
+      { option: "Alpha Centauri B", correct: false },
+    ],
+  },
+];
