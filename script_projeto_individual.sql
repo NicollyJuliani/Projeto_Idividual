7e5a0df0d8a286f544a3eaac31927e86795e89ca
Nicolly Juliani <nicollyjuliane2004@gmail.com>
2023-05-20 22:29:21 -0300

    tea desempenho e tela ranking

--- script_projeto_individual.sql
+++ script_projeto_individual.sql
@@ -24,6 +24,10 @@ nomeJogo varchar(45),
 pontuacaoMax INT
 )auto_increment = 100;
 
+INSERT INTO jogo VALUES 
+(null, "Quiz", "100"),
+(null, "Quiz 2", "100");
+
 
 CREATE TABLE pontosUsuario(
 idPontos INT PRIMARY KEY auto_increment,
@@ -34,3 +38,13 @@ dtPontuacao DATE,
 constraint comp_fkJogo foreign key (FkJogo) references jogo(idJogo),
 constraint comp_fkUsuario foreign key (fkUsuario) references usuario(idUsuario)
 )auto_increment = 1000;
+
+desc pontosUsuario;
+ALTER TABLE pontosUsuario modify column dtPontuacao datetime default current_timestamp;
+
+INSERT INTO pontosUsuario VALUES
+(null, 100, 3, 20, default),
+(null, 100, 4, 100, default);
+
+SELECT usuario.nickname, pontosUsuario.pontuacao FROM usuario JOIN pontosUsuario ON fkUsuario = idUsuario
+WHERE nickname = 'nicolly' limit 1;
